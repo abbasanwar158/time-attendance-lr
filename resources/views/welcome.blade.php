8551a0e0119bd7a06dd63c4265bade92a2a66b82
abbasanwar158 <abbas.anwar@devbox.co>
2021-11-22 11:46:51 +0500

    app js and app css issue reoslve

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Time Attendance</title>
-        <link rel="stylesheet" href="{{ asset('./css/app.css') }}"></link>
+        <link rel="stylesheet" href="http://time-attendance-lr.herokuapp.com/css/app.css"></link>
     </head>
     <body>
         <div id="app"></div>
-        <script src="{{ asset('./js/app.js') }}"></script>
+        <script src="http://time-attendance-lr.herokuapp.com/js/app.js"></script>
     </body>
 </html>
