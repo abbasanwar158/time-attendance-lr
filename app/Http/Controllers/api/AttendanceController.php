4702c5ba075b21596165ba233cee394f5d886f16
abbasanwar158 <abbas.anwar@devbox.co>
2021-11-24 19:21:23 +0500

    attendance Fix

--- app/Http/Controllers/api/AttendanceController.php
+++ app/Http/Controllers/api/AttendanceController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use App\Models\Attendance;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use DateTime;
 
 class AttendanceController extends Controller
 {
@@ -15,7 +17,23 @@ class AttendanceController extends Controller
      */
     public function index()
     {
-        return Attendance::all();
+        $data = Attendance::join('employees', 'employee_external_id', '=', 'attendances.employee_id')
+        ->orderBy('date', 'DESC')
+        ->get(['employees.name', 'attendances.id', 'attendances.date', 'attendances.checkin', 'attendances.checkout', 'attendances.created_at']);
+        foreach($data as $key => $value ){
+            $date1 = new DateTime($value->checkin);
+            $date2 = new DateTime($value->checkout);
+            $difference = $date1->diff($date2);
+            $diffInMinutes = sprintf("%02d", $difference->i);
+            $diffInHours   = sprintf("%02d", $difference->h);
+            $result = $diffInHours . ':' . $diffInMinutes;
+            $value->timeSpend = $result;
+            $day = date('l', strtotime($value->date));
+            $value->day = $day;
+            $value->checkin = date("g:i a", strtotime($value->checkin));
+            $value->checkout = date("g:i a", strtotime($value->checkout));
+        }
+        return $data;
     }
 
     /**
@@ -23,9 +41,21 @@ class AttendanceController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function todayAttendance()
     {
-        //
+        $today = Carbon::today();
+        $data = Attendance::where('date', '=', $today)->join('employees', 'employee_external_id', '=', 'attendances.employee_id')
+        ->get(['employees.name', 'attendances.id', 'attendances.date', 'attendances.checkin', 'attendances.checkout']);
+        foreach($data as $key => $value ){
+            $date1 = new DateTime($value->checkin);
+            $date2 = new DateTime($value->checkout);
+            $difference = $date1->diff($date2);
+            $diffInMinutes = sprintf("%02d", $difference->i);
+            $diffInHours   = sprintf("%02d", $difference->h);
+            $result = $diffInHours . ':' . $diffInMinutes;
+            $value->timeSpend = $result;
+        }
+        return $data;
     }
 
     /**
