cd6855eb2c21ff9c7763bff714430af341b82f3f
abbasanwar158 <abbas.anwar@devbox.co>
2021-12-24 11:21:56 +0500

    test mail from heroku

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,8 +30,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call('App\Http\Controllers\api\AttendanceController@index')->dailyAt('11:30')->timezone('Asia/Karachi');
        
-        $schedule->call('App\Http\Controllers\api\AttendanceController@AttendanceAlert')->dailyAt('23:33')->timezone('Asia/Karachi');
+        // $schedule->call('App\Http\Controllers\api\AttendanceController@AttendanceAlert')->dailyAt('23:33')->timezone('Asia/Karachi');
     }
 
     /**
