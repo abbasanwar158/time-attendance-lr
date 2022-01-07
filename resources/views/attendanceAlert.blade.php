de9026d448db9226a1e565ab00dbdbec0b82b9aa
abbasanwar158 <abbas.anwar@devbox.co>
2022-01-07 18:21:37 +0500

    change text of mail

--- resources/views/attendanceAlert.blade.php
+++ resources/views/attendanceAlert.blade.php
@@ -1,4 +1,5 @@
 Dear <b>{{"$employeeName"}}</b>!
 <br>
-Today you spent {{"$Time"}}<br>
-Please complete 9 hours daily
+Today you spent {{"$Time"}}
+<br>
+Every person should be in office for 9 hours that is 8 working hours and 1 break hour, unless informed about early leaving or short leave!
